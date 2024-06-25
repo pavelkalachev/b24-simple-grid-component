8733fa2b123fc8930a75f7301d9fd175f679f2bf
Pavel Kalachev <hoffman.ozr@gmail.com>
2024-06-25 21:31:53 +0500

    Show tasks via bitrix:main.ui.grid

--- pkalachev/simple.grid/class.php
+++ pkalachev/simple.grid/class.php
@@ -5,10 +5,37 @@ class SimpleGridComponent extends CBitrixComponent
     public function executeComponent()
     {
         $gridId = 'SIMPLE_GRID';
+        $gridRows = [];
 
         $gridFilter = $this->getFilterFields();
+        $entityRepository = $this->getEntityRepository();
+
+        $items = $entityRepository::getList([
+            'select' => ['*']
+        ]);
+
+        while ($item = $items->fetchObject()) {
+            $gridRows[] = [
+                'id' => $item->getId(),
+                'columns' => [
+                    'ID' => $item->getId(),
+                    'TITLE' => $item->getTitle(),
+                    'CREATED_DATE' => $item->getCreatedDate()
+                ]
+            ];
+        }
+
+        $this->arResult['GRID_ID'] = $gridId;
+        $this->arResult['GRID_FILTER'] = $gridFilter;
+        $this->arResult['GRID_COLUMNS'] = $this->getGridColumns();
+        $this->arResult['GRID_ROWS'] = $gridRows;
 
         $this->includeComponentTemplate();
+    }
+
+    protected function getEntityRepository()
+    {
+        return '\Bitrix\Tasks\Internals\TaskTable';
     }
 
     protected function getFilterFields(): array
@@ -26,6 +53,27 @@ class SimpleGridComponent extends CBitrixComponent
                 'type' => 'date',
                 'default' => true
             ]
+        ];
+    }
+
+    protected function getGridColumns()
+    {
+        return [
+            [
+                'id' => 'ID',
+                'name' => 'ID задачи',
+                'default' => true
+            ],
+            [
+                'id' => 'TITLE',
+                'name' => 'Заголовок',
+                'default' => true
+            ],
+            [
+                'id' => 'CREATED_DATE',
+                'name' => 'Дата создания',
+                'default' => true
+            ],
         ];
     }
 }
--- pkalachev/simple.grid/templates/.default/template.php
+++ pkalachev/simple.grid/templates/.default/template.php
@@ -1,3 +1,28 @@
-<?php
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 
-echo __FILE__;
+<?php
+$APPLICATION->IncludeComponent('bitrix:main.ui.grid', '', [
+    'GRID_ID' => $arResult['GRID_ID'],
+    'COLUMNS' => $arResult['GRID_COLUMNS'],
+    'ROWS' => $arResult['GRID_ROWS'],
+    'AJAX_MODE' => 'Y',
+    'AJAX_ID' => \CAjax::getComponentID('bitrix:main.ui.grid', '.default', ''),
+    'AJAX_OPTION_JUMP' => 'N',
+    'SHOW_ROW_CHECKBOXES' => true,
+    'SHOW_CHECK_ALL_CHECKBOXES' => true,
+    'SHOW_ROW_ACTIONS_MENU' => true,
+    'SHOW_GRID_SETTINGS_MENU' => true,
+    'SHOW_NAVIGATION_PANEL' => true,
+    'SHOW_PAGINATION' => true,
+    'SHOW_SELECTED_COUNTER' => false,
+    'SHOW_TOTAL_COUNTER' => false,
+    'SHOW_PAGESIZE' => true,
+    'SHOW_ACTION_PANEL' => true,
+    'ALLOW_COLUMNS_SORT' => true,
+    'ALLOW_COLUMNS_RESIZE' => true,
+    'ALLOW_HORIZONTAL_SCROLL' => true,
+    'ALLOW_SORT' => true,
+    'ALLOW_PIN_HEADER' => true,
+    'AJAX_OPTION_HISTORY' => 'N',
+    "ENABLE_COLLAPSIBLE_ROWS" => true
+]); ?>
