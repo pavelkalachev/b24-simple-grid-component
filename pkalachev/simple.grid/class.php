463189cbf94d18e9f3b2afd1d1885d3faea6cda9
Pavel Kalachev <hoffman.ozr@gmail.com>
2024-06-25 21:02:54 +0500

    Create get filter fields method

--- pkalachev/simple.grid/class.php
+++ pkalachev/simple.grid/class.php
@@ -1,9 +1,31 @@
 <?php
 
-class SimpleGrid extends CBitrixComponent
+class SimpleGridComponent extends CBitrixComponent
 {
     public function executeComponent()
     {
+        $gridId = 'SIMPLE_GRID';
+
+        $gridFilter = $this->getFilterFields();
+
         $this->includeComponentTemplate();
+    }
+
+    protected function getFilterFields(): array
+    {
+        return [
+            [
+                'id' => 'TITLE',
+                'name' => 'Название',
+                'type' => 'date',
+                'default' => true
+            ],
+            [
+                'id' => 'CREATED_DATE',
+                'name' => 'Дата создания',
+                'type' => 'date',
+                'default' => true
+            ]
+        ];
     }
 }
